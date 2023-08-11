3a2cbcf6d7b246f316f4dff56b39e37f74ea069e
ohinhyuk <8156217@naver.com>
2023-08-12 04:09:18 +0900

    바닐라 js에서의 redux 스터디

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const { createStore, combineReducers } = require("redux");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+const CONCAT = "CONCAT";
+
+number.innerText = 0;
+
+const toDoReducer = (state = [], action) => {
+  switch (action.type) {
+    case CONCAT:
+      return state.concat(action.text);
+    default:
+      return state;
+  }
+};
+
+const countReducer = (state = 0, action) => {
+  switch (action.type) {
+    case ADD:
+      return state + 1;
+    case MINUS:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+const reducer = combineReducers({
+  countReducer,
+  toDoReducer,
+});
+
+const countStore = createStore(reducer, {
+  countReducer: 0,
+  toDoReducer: ["HELLO"],
+});
+
+console.log(countStore.getState());
+
+const onChange = () => {
+  number.innerText = countStore.getState().countReducer;
+  console.log(countStore.getState());
+};
+
+countStore.subscribe(onChange);
+
+add.addEventListener("click", () =>
+  countStore.dispatch({ type: CONCAT, text: "HELLO" })
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
